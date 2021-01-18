f731347db181d82a412fc8f23146fe28430710e1
Mukesh Soni <msony552@gmail.com>
2021-01-18 16:45:17 +0530

    Assignment1Q2 & Assignment1Q3

---
+++ Core Java 8/src/com/corejava/Assignment1Q3.java
@@ -0,0 +1,16 @@
+package com.corejava;
+
+import java.lang.Math;
+
+class SiCi {
+    public double simpleInterest(double principalAmount,int time,double interestRate){
+        return (principalAmount * interestRate * time)/100;
+    }
+    public double compoundInterest(double principalAmount,int time,double interestRate){
+        double r = interestRate / 100;
+        return principalAmount * Math.pow(1 + r, time) - principalAmount;
+    }
+}
+public class Assignment1Q3 {
+    public static void main (String args[]) {}
+}
