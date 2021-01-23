24ea01c74ecac6e093284bce815a6d3ffaa24f02
Mukesh Soni <msony552@gmail.com>
2021-01-23 20:27:06 +0530

    Assignment1Q7 & Assignment1Q8

---
+++ Core Java 8/src/com/corejava/Assignment1Q7.java
@@ -0,0 +1,18 @@
+package com.corejava;
+
+class SearchArray{
+    public boolean searchArray(int[] arr,int toCheckValue){
+        int size = 15;
+        boolean ans = false;
+        for(int i=0, j=size-1;i!=j;++i,--j){
+            if(arr[i] == toCheckValue || arr[j] == toCheckValue) ans = true;
+        }
+        return ans;
+    }
+}
+public class Assignment1Q7 {
+    public static void main(String[] args) {
+        int arr[] = { 5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
+        int valueToCheck = 19;
+    }
+}
---
+++ Core Java 8/src/com/corejava/Assignment1Q8.java
@@ -0,0 +1,28 @@
+package com.corejava;
+
+class BubbleSort{
+
+    public int[] bubbleSort(int arr[]) {
+        int size = arr.length;
+        for(int i=0;i<size-1;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
+public class Assignment1Q8 {
+    public static void main(String args[]) {
+        int arr[] = {5,12,14,6,78,19,1,23,26,35,37,7,52,86,47};
+        BubbleSort bubbleSort = new BubbleSort();
+        arr = bubbleSort.bubbleSort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
