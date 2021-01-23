c5782a211b816602f8edde893f99e2a38b9cd186
Mukesh Soni <msony552@gmail.com>
2021-01-23 12:02:08 +0530

    Assignment2Q2

---
+++ Core Java 8/src/com/corejava/Assignment2Q2.java
@@ -0,0 +1,35 @@
+package com.corejava;
+
+import java.util.ArrayList;
+
+class Manager extends Assignment2Q2 {
+    @Override
+    public int getSalary(int salary) {
+        int incentive = 5000;
+        return salary + incentive;
+    }
+}
+
+class Labour extends Assignment2Q2 {
+    @Override
+    public int getSalary(int salary) {
+        int overtime = 500;
+        return salary + overtime;
+    }
+}
+
+public class Assignment2Q2 {
+    int salary = 10000;
+    public int getSalary(int salary){
+        return salary;
+    }
+    public int totalEmployeesSalary(ArrayList<Integer> employeeSalaries){
+        int total=0;
+        int size = employeeSalaries.size();
+        for(int i=0;i<size;++i){
+            total+=employeeSalaries.get(i);
+        }
+        return total;
+    }
+    public static void main(String[] args) {}
+}
