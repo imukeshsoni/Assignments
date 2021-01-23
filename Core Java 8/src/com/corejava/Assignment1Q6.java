7602dc52dee0c360164933383982cbe9da47a38f
Mukesh Soni <msony552@gmail.com>
2021-01-23 20:03:23 +0530

    Assignment1Q6

---
+++ Core Java 8/src/com/corejava/Assignment1Q6.java
@@ -0,0 +1,25 @@
+package com.corejava;
+
+class Login{
+    String userId = "Ajay",password = "password";
+    int attempt=0;
+    public String loginUser(String user, String pass) {
+
+        if(attempt == 3){
+            return "You have entered wrong credentials 3 times\nContact Admin";
+        }
+
+
+        if(user == userId && pass == password){
+            attempt=0;
+            return "Welcome "+ user;
+        }else{
+            attempt++;
+            return "You have entered wrong credentials ,please enter the right credentials.";
+        }
+    }
+
+}
+public class Assignment1Q6 {
+    public static void main(String[] args) {}
+}
