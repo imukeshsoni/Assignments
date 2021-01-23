d63a7308dc17d8f23d402459f32e39cdc839edd0
Mukesh Soni <msony552@gmail.com>
2021-01-23 12:35:09 +0530

    Assignment2Q4

---
+++ Core Java 8/src/com/corejava/Assignment2Q4.java
@@ -0,0 +1,25 @@
+package com.corejava;
+
+abstract class EmptyClass{
+
+}
+
+abstract class AbstractClass{
+    void show(){
+        System.out.print("abstruct class method called!");
+    }
+}
+
+public class Assignment2Q4 extends AbstractClass{
+
+    @Override
+    void show(){
+        System.out.println("derived class function called!");
+    }
+
+    public static void main(String srgs[]){
+        Assignment2Q4 obj = new Assignment2Q4();
+        obj.show();
+
+    }
+}
