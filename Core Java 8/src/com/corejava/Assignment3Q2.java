286c3973771d360fba8493698b9dff904e449324
Mukesh Soni <msony552@gmail.com>
2021-01-28 12:12:54 +0530

    Assignment3Q2

---
+++ Core Java 8/src/com/corejava/Assignment3Q2.java
@@ -0,0 +1,39 @@
+package com.corejava;
+import java.util.*;
+public class Assignment3Q2 {
+    public static void main(String[] args) {
+
+
+    }
+    public static LinkedHashSet ordered(LinkedHashSet linkedHashSet){
+        LinkedHashSet<String> lh=new LinkedHashSet();
+        lh.add("A");
+        lh.add("B");
+        lh.add("E");
+        lh.add("F");
+        lh.add("C");
+        lh.add("D");
+        Iterator<String> i=lh.iterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next()); //This will print elements in maintained order
+        }
+
+        return linkedHashSet;
+    }
+    public static HashSet unordered(HashSet hashSet){
+        HashSet<String> lh=new HashSet();
+        lh.add("A");
+        lh.add("B");
+        lh.add("E");
+        lh.add("F");
+        lh.add("C");
+        lh.add("D");
+        Iterator<String> i=lh.iterator();
+        while(i.hasNext())
+        {
+            System.out.println(i.next()); //This will print elements in unmaintained order
+        }
+        return hashSet;
+    }
+}
