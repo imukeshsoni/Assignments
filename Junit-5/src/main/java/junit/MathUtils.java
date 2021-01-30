73664a08b9347609754a0927dd16a22985898925
Mukesh Soni <msony552@gmail.com>
2021-01-30 15:51:14 +0530

    Sample test run

--- Junit-5/src/main/java/junit/MathUtils.java
+++ Junit-5/src/main/java/junit/MathUtils.java
@@ -1,4 +1,9 @@
+package junit;
+
+import org.junit.jupiter.api.Test;
+
 public class MathUtils {
+    @Test
     public int add(int a, int b){
         return a+b;
     }
---
+++ Junit-5/src/test/java/junit/MathUtilsTest.java
@@ -0,0 +1,11 @@
+package junit;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+    @Test
+    void test(){
+        System.out.println("Test run successfully");
+    }
+}
