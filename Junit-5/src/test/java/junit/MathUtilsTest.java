e2f8a5c602bd67492d73f6bcc7ad40193ba131dc
Mukesh Soni <msony552@gmail.com>
2021-01-30 16:02:26 +0530

    Assertions added in test file

--- Junit-5/src/test/java/junit/MathUtilsTest.java
+++ Junit-5/src/test/java/junit/MathUtilsTest.java
@@ -1,10 +1,16 @@
 package junit;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.gen5.api.Assertions.assertEquals;
 
 public class MathUtilsTest {
     @Test
     void test(){
-        System.out.println("Test run successfully");
+        MathUtils mathUtils = new MathUtils();
+        int expected = 3;
+        int actual = mathUtils.add(1,1);
+        assertEquals(expected, actual);
     }
 }
