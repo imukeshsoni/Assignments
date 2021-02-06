0cdc7721a022d2157235c0c34c293fd773c54588
Mukesh Soni <msony552@gmail.com>
2021-02-06 12:02:08 +0530

    @Required annotation used in Shape interface (Deprecated) - spring.xml

--- Spring-Core/src/main/java/Question5/Test.java
+++ Spring-Core/src/main/java/Question5/Test.java
@@ -1,7 +1,12 @@
 package Question5;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
     public static void main(String[] args) {
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Shape shape = (Shape) context.getBean("circle");
+        shape.draw();
     }
 }
