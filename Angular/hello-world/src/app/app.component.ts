6d68de6852942d2e30a36beedfcf68fc263e2410
Mukesh Soni <msony552@gmail.com>
2021-03-03 17:38:35 +0530

    component interaction files

--- Angular/hello-world/src/app/app.component.ts
+++ Angular/hello-world/src/app/app.component.ts
@@ -6,8 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public title = "Mukesh";
-  public name = "Mukesh Soni";
-  public message = "";
-
+  title = 'hello-world';
 }
